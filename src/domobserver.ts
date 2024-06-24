4cf1eae1f43d8f4ada41853db0384eeaf31fc883
ght901 <msnt901@gmail.com>
2024-06-24 16:22:07 -0700

    Change supressSelectionUpdates timeout

--- src/domobserver.ts
+++ src/domobserver.ts
@@ -118,7 +118,7 @@ export class DOMObserver {
 
   suppressSelectionUpdates() {
     this.suppressingSelectionUpdates = true
-    setTimeout(() => this.suppressingSelectionUpdates = false, 50)
+    setTimeout(() => this.suppressingSelectionUpdates = false, 1000)
   }
 
   onSelectionChange() {
